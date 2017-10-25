9d7d87633c49edec432e1b197889dded3b0c60fb
NilugedeMel <32806061+NilugedeMel@users.noreply.github.com>
2017-10-25 10:01:37 -0400

    Add files via upload
    user input

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,21 @@
+//  import libraries
+import java.util.Scanner;
+
 public class hello_world {
 
+        //  global variables
+        String phrase = "";
+
         public static void main (String [] args) {
-                System.out.println ("hello world");
+                hello_world start = new hello_world ();
+                start.user_input ();
+        }
+
+        void user_input () {
+                Scanner input = new Scanner (System.in);
+                System.out.println ("Enter a phrase");
+                phrase = input.nextLine ();
+                System.out.println ("Your phrase was " + phrase);
         }
 
 }
